902c04d554a303dbe52d5595c10cb0bc59e51703
Beataselman@msn.com <Beataselman@msn.com>
2025-11-06 23:11:13 +0100

    Poprawiono URLe - zmieniono vacatures na jobs

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,8 @@
 ﻿from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
     path('candidates/', include('candidates.urls', namespace='candidates')),
-     path('vacatures/', include('Ogloszeniafirm.urls', namespace='ogloszeniafirm')),  # używamy namespace zgodnego z app_name
+    path('jobs/', include('Ogloszeniafirm.urls', namespace='ogloszeniafirm')),  # ⬅️ Zmieniłem z 'vacatures/' na 'jobs/'
 ]
